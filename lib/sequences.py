7e4c46d628f5f1586612e1e9401d2c669780bc39
Ian Gathua <gathuambui@gmail.com>
2025-03-17 19:09:01 +0300

    Final commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    
+    fib_sequence = [0, 1]
+    for _ in range(2,length):
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+
+    print(fib_sequence)
+    
